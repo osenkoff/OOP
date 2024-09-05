4c83fc937d51c9cfecb8ef5c82c4b2b7ae4d0d10
ArtyomMasen <osenkoffartyom@gmail.com>
2024-09-05 12:04:55 +0300

    Fixed task1.2 and added tests

--- lab1/lib/task2/compare.dart
+++ lab1/lib/task2/compare.dart
@@ -1,22 +1,12 @@
-import 'dart:io';
-
 class CompareFiles {
   static int _line = 0;
 
   Future<bool> isEqual({
-    required String firstFilePath,
-    required String secondFilePath,
+    required List<String> firstFileContent,
+    required List<String> secondFileContent,
   }) async {
-    final firstFile = File(firstFilePath);
-    final secondFile = File(secondFilePath);
-
-    areFilesExist(firstFile: firstFile, secondFile: secondFile);
-
-    final firstFileContent = await firstFile.readAsLines();
-    final secondFileContent = await secondFile.readAsLines();
-
     for (_line; _line < firstFileContent.length; _line++) {
-      if (firstFileContent[_line].contains(secondFileContent[_line])) {
+      if (firstFileContent[_line] == secondFileContent[_line]) {
         continue;
       } else {
         return false;
@@ -25,37 +15,5 @@ class CompareFiles {
     return true;
   }
 
-  Future<bool> areFilesExist({
-    required File firstFile,
-    required File secondFile,
-  }) async {
-    if (!await firstFile.exists()) {
-      throw Exception('first file do not exist! Create it or enter right path');
-    } else if (!await secondFile.exists()) {
-      throw Exception('second file do not exist! Create it or enter right path');
-    } else {
-      return true;
-    }
-  }
-
   static int get line => _line;
-}
-
-void main(List<String> arguments) async {
-  final comparingFiles = CompareFiles();
-  final filesAreEqual = await comparingFiles.isEqual(
-    firstFilePath: arguments[0],
-    secondFilePath: arguments[1],
-  );
-
-  try {
-    if (filesAreEqual) {
-      print('Files are equal!');
-    } else {
-      final int mismatchLine = CompareFiles.line;
-      throw 'Files are not equals! Line number is ${mismatchLine + 1}';
-    }
-  } catch (e) {
-    print(e.toString());
-  }
 }
---
+++ lab1/test/compare_files.dart
@@ -0,0 +1,89 @@
+import 'package:lab1/task2/compare.dart';
+import 'package:test/test.dart';
+
+void main() {
+  late CompareFiles compareFiles;
+
+  setUp(() {
+    compareFiles = CompareFiles();
+  });
+
+  group('When strings are different', () {
+    test('One empty and one filled strings should return false', () async {
+      // Arrange
+      var firstFileContent = [''];
+      var secondFileContent = ['filled string'];
+
+      // Act
+      var isStringsNotEqual = compareFiles.isEqual(
+        firstFileContent: firstFileContent,
+        secondFileContent: secondFileContent,
+      );
+
+      // Assert
+      expect(await isStringsNotEqual, false);
+    });
+
+    test('One filled and one empty strings should return false', () async {
+      // Arrange
+      var firstFileContent = ['filled string'];
+      var secondFileContent = [''];
+
+      // Act
+      var isOppositeStringsNotEqual = compareFiles.isEqual(
+        firstFileContent: firstFileContent,
+        secondFileContent: secondFileContent,
+      );
+
+      // Assert
+      expect(await isOppositeStringsNotEqual, false);
+    });
+
+    test('Two strings with different content should return false', () async {
+      // Arrange
+      var firstFileContent = ['First string content'];
+      var secondFileContent = ['Second string content'];
+
+      // Act
+      var isDifferentStringsNotEqual = compareFiles.isEqual(
+        firstFileContent: firstFileContent,
+        secondFileContent: secondFileContent,
+      );
+
+      // Assert
+      expect(await isDifferentStringsNotEqual, false);
+    });
+  });
+
+  group('When strings are same', () {
+    test('Two strings with same content should return true', () async {
+      // Arrange
+      var firstFileContent = ['string content'];
+      var secondFileContent = ['string content'];
+
+      // Act
+      var isSameStringsEqual = compareFiles.isEqual(
+        firstFileContent: firstFileContent,
+        secondFileContent: secondFileContent,
+      );
+
+      // Assert
+      expect(await isSameStringsEqual, true);
+    });
+
+    test('Two empty strings should return true', () async {
+      // Arrange
+      var firstFileContent = [''];
+      var secondFileContent = [''];
+
+      // Act
+      var isStringsEqual = compareFiles.isEqual(
+        firstFileContent: firstFileContent,
+        secondFileContent: secondFileContent,
+      );
+
+      // Assert
+      expect(await isStringsEqual, true);
+    });
+  });
+}
